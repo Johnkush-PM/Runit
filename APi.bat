40bfb13f148d0ac658d7a000d32977a6418d065c
Johnkush-PM <57130315+Johnkush-PM@users.noreply.github.com>
2020-08-11 21:40:30 +0530

    Add files via upload

---
+++ APi.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"C:\Program Files\SmartBear\ReadyAPI-3.3.1\bin\testrunner.bat" -r -a -j -J "-fG:\JUNIT Result" "-RJUnit-Style HTML Report" -FXML "-EDefault environment" C:\Users\ADMIN\Downloads\Project-4-readyapi-project
+PAUSE
