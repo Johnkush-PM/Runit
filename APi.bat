781dac40aa8fb3d284c3c87e1c15802628870e5e
Johnkush-PM <57130315+Johnkush-PM@users.noreply.github.com>
2020-08-11 22:34:10 +0530

    Delete APi.bat

--- APi.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-"C:\Program Files\SmartBear\ReadyAPI-3.3.1\bin\testrunner.bat" -r -a -j -J "-fG:\JUNIT Result" "-RJUnit-Style HTML Report" -FXML "-EDefault environment" C:\Users\ADMIN\Downloads\Project-4-readyapi-project
-PAUSE
