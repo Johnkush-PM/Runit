f0342a2fb87886dc9bcda3e48af00ea77128647f
Johnkush-PM <57130315+Johnkush-PM@users.noreply.github.com>
2020-08-12 13:58:15 +0530

    Add files via upload

---
+++ sec.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+"C:\Program Files\SmartBear\ReadyAPI-3.3.1\bin\securitytestrunner.bat" -r -a -j -f"G:\Security JUNIT" -R"Security Issues Report" -FPDF -E"Default environment" C:\Users\ADMIN\Downloads\Project-4-readyapi-project
+PAUSE
